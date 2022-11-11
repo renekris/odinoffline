c133f05dbfc5436f7dd1e54df4f7cd047eb6b110
Renekris <renekrispohlak@gmail.com>
2022-11-11 06:46:37 +0200

    Add size, prepend & refactor to use callback

--- 36-linked-lists/linked-lists.js
+++ 36-linked-lists/linked-lists.js
@@ -2,18 +2,31 @@ class LinkedList {
   constructor() {
     this.head;
     this.tail;
+
+    this.size;
   }
 
   get tail() {
     if (this.head === undefined) return this.head;
 
-    let currentPointer = this.head;
-    while (currentPointer !== null) {
-      if (currentPointer.next === null || undefined) {
-        return currentPointer;
+    let tailNode = this.head;
+    this.#iterateNodes((node) => {
+      if (node.next === null || undefined) {
+        tailNode = node;
       }
-      currentPointer = currentPointer.next;
-    }
+    })
+
+    return tailNode;
+  }
+
+  get size() {
+    if (this.head === undefined) return 0;
+
+    let nth = 0;
+    this.#iterateNodes(() => {
+      nth += 1;
+    });
+    return nth;
   }
 
   append(value) {
@@ -22,13 +35,34 @@ class LinkedList {
       return this.head;
     }
 
-    return this.#createNode(this.tail, value);
+    const tailNode = this.tail;
+    tailNode.next = this.#createNode(value);
+    return tailNode.next;
   }
 
-  #createNode(currentPointer, value) {
+  prepend(value) {
+    const newNode = this.#createNode(value);
+    if (this.head === undefined) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    return this.head;
+  }
+
+  #iterateNodes(callback) {
+    let currentPointer = this.head;
+    while (currentPointer !== null) {
+      callback(currentPointer);
+      currentPointer = currentPointer.next;
+    }
+  }
+
+  #createNode(value) {
     const newNode = new Node(value);
-    currentPointer.next = newNode;
-    return currentPointer.next;
+    return newNode;
   }
 }
 
@@ -41,16 +75,22 @@ class Node {
 
 const list = new LinkedList();
 
+console.log(list.size);
+list.prepend('-1');
 list.append('0');
 list.append('1');
 console.log(list.head);
 list.append('2');
 console.log(list.tail);
 list.append('3');
+list.prepend('-2');
+console.log(list.head);
 list.append('4');
 console.log(list.tail);
 list.append('5');
+list.prepend('-3');
 list.append('6');
 list.append('7');
 console.log(list.head);
 console.log(list.tail);
+console.log(list.size);
